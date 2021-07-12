4c4b573ce3b4d8ebad77acb2396ed9589cbc7529
Pixelaze <56383696+Pixelaze@users.noreply.github.com>
2021-07-12 16:02:03 +0500

    Update MenuControls.cs

--- Assets/MenuControls.cs
+++ Assets/MenuControls.cs
@@ -7,7 +7,7 @@ public class MenuControls : MonoBehaviour
 {
     public void PlayPressed()
     {
-        SceneManager.LoadScene("Game");
+        SceneManager.LoadScene("SampleScene");
     }
     public void ExitPressed()
     {
